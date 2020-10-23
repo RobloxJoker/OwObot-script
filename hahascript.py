7fd606b9a0b50124ef9df64476c1ea66ceaed3ab
RobloxJoker <72589257+RobloxJoker@users.noreply.github.com>
2020-10-23 10:19:03 +0530

    Add files via upload

--- hahascript.py
+++ hahascript.py
@@ -2,19 +2,41 @@ from pynput.keyboard import Key,Controller
 import time 
 import random
 
+
+
+
+
+#pip install pynput
+
 kbd = Controller()
 
 input('press Enter to start')
 
 def pause(t):
+
     while (t>0):
         print(f'Time Left:{t}')
         t-=1
         time.sleep(1)
 
+
+
 while True:
-    kbd.type('oh')
+    nouns = ("puppy" , "rabbit" , "girl" , "monkey", "Kkgameryt", "MCguitardude", "Rebbo2818")
+    verbs = ("runs" , "hits" , "jumps", "drives", "barfs", "MLG", "Swim") 
+    adv = ("crazily " , "dutifully" , "foolishly", "merrily", "occasionally", "in Lava", "was shot")
+    adj = ("adorable" , "clueless" , "dirty", "stupid", "and died", "lmfaooo", "By Rebbo2818")
+
+    num = (random.randint(0,6))
+    num1 = (random.randint(0,6))
+    num2 = (random.randint(0,6))
+    num3 = (random.randint(0,6))
+    kbd.type(nouns[num] + ' ' + verbs[num1] + ' ' + adv[num2] + ' ' + adj[num3])
+	#this is the text that is typed out, leave the quotation marks
+
     kbd.press(Key.enter)
     kbd.release(Key.enter)
     print('Command Sent')
-    pause(random.randint(17,24))
+    pause(random.randint(30,40))
+
+
